79b85e439d0a838610afd2e9af89370c9de42032
sarahmfloyd <106851309+sarahmfloyd@users.noreply.github.com>
2023-03-20 19:44:08 -0400

    more uploads

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,17 @@
+/*!
+ * Item: CodeX
+ * Description:  Personal Portfolio / Resume / CV / vCard Template
+ * Author/Developer: Exill
+ * Author/Developer URL: https://themeforest.net/user/exill
+ * Version: v1.0.0
+ * License: Themeforest Standard Licenses: https://themeforest.net/licenses
+ */
+
 /*----------- CUSTOM JS SCRIPTS -----------*/
 
 (function($) {
   'use strict';
-  $(function(){
+  $(function() {
     // Code here executes When the DOM is loaded...
   });
   $(window).on('load', function() {
